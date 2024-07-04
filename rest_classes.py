6e87f022fc37baca46686ca2d61a4c7849577b23
svetasolog <95722857+svetasolog@users.noreply.github.com>
2024-07-04 13:26:23 -0400

    Update rest_classes.py

--- rest_classes.py
+++ rest_classes.py
@@ -1,22 +1,29 @@
+class Menu():
+    def __init__(self):
+        self.name={}
+        self.items=[]
 class Item():
     def __init__(self, name, price, cals):
         self.name=name
         self.price=price
         self.cals=cals
+    def get_name(self):
+        return self.name
 
 class Drink(Item):
     def __init__(self, name, price, cals):
         super().__init__(name, price, cals)
 
 class ColdDrink(Drink):
-    def __init__(self, name, price, cals, options):
+    def __init__(self, name, price, cals, options=[]):
         super().__init__(name, price, cals)
         self.options = options
-
+        print(f"{self.name}, {self.price}, {self.cals}, {self.options}")
 class HotDrink(Drink):
     def __init__(self, name, price, cals, options):
         super().__init__(name, price, cals)
         self.options = options
+        print(f"{self.name}, {self.price}, {self.cals}, {self.options}")
 
 class Food(Item):
     def __init__(self, name, price, cals, descr):
@@ -37,4 +44,3 @@ class Sandwich(Food):
     def __init__(self, name, price, cals, descr, prep):
         super().__init__(name, price, cals, descr)
         self.prep = prep
-
