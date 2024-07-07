c789aeae98352b24ac2bdefba7985fc29c513bad
svetasolog <95722857+svetasolog@users.noreply.github.com>
2024-07-06 21:36:36 -0400

    Update rest_classes.py

--- rest_classes.py
+++ rest_classes.py
@@ -11,6 +11,10 @@ class Item():
         self.cals=cals
     def get_name(self):
         return self.name
+    def get_price(self):
+        return self.price
+    def get_cals(self):
+        return self.cals
 
 class Drink(Item):
     def __init__(self, name, price, cals):
@@ -19,12 +23,20 @@ class Drink(Item):
 class ColdDrink(Drink):
     def __init__(self, name, price, cals):
         super().__init__(name, price, cals)
-        self.options = ['no ice','ice','light ice','extra ice']
+        self.options = {"Ice":['no ice','ice','light ice','extra ice']}
+    def show_category(self):
+        return "Cold Drink"
+    def get_options(self):
+        return self.options
 
 class HotDrink(Drink):
     def __init__(self, name, price, cals, options):
         super().__init__(name, price, cals)
         self.options = options
+    def show_category(self):
+        return "Hot Drink"
+    def get_options(self):
+        return self.options
 
 
 class Food(Item):
@@ -36,13 +48,19 @@ class Soup(Food):
     def __init__(self, name, price, cals, descr, bread):
         super().__init__(name, price, cals, descr)
         self.bread = bread
+    def show_category(self):
+        return "Soup"
 
 class Salad(Food):
     def __init__(self, name, price, cals, descr, dressing):
         super().__init__(name, price, cals, descr)
         self.dressing = dressing
+    def show_category(self):
+        return "Salad"
 
 class Sandwich(Food):
     def __init__(self, name, price, cals, descr, prep):
         super().__init__(name, price, cals, descr)
         self.prep = prep
+    def show_category(self):
+        return "Sandwich"
